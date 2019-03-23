c5fd85fe495a1d528c75ec87d38fbaf7e786ff6d
EliezerSoaresCruz <48848565+EliezerSoaresCruz@users.noreply.github.com>
2019-03-23 15:37:55 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
-if ($_POST['acao']=="iserir"){
-	inserirpessoa();
+if ($_POST['acao']=='inserir') {
+	$data = date('d-m-Y',strtotime($_POST['data']));
+	print('<b>Seu nome é: '.$_POST['nome'].'<br>');
+	print('Seu e-mail é: '.$_POST['email'].'<br>');
+	print('Seu endereço é: '.$_POST['endereco'].'<br>');
+	print('Nasceu em: '.$data.'</b>');
 }
-function inserirpessoa(){
-	$banco = new mysqli("localhost","root","", "cliente");
-	$sql
-} 
 ?>
