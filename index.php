9e984c3e236a5c2ca9c1f4dd34f339b0bf48b5eb
EliezerSoaresCruz <48848565+EliezerSoaresCruz@users.noreply.github.com>
2019-03-23 15:45:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head> <meta charset="utf-8">
-	<title>Crude</title>
+<head><meta charset="utf-8">
+	<title> Crud </title>
 	<style type="text/css">
 		h1{
-			text-align-last: center;
+
 			font-family: verdana;
+			text-align: center;
+
 		}
 	</style>
 </head>
-
-
 <body>
-
-	<h1> Cadastro de cliente </h1>
-	<center><form name="Dados Cliente" action="conexao.php" method="POST">
-		<table border="1">
+	<h1> Tabela cadastro cliente </h1>
+	<center>
+	<form name="dados cliente" action="conexao.php" method="POST">
+		<table border="1"> 
 			<tr>
-				<td>Nome:</td>
-				<td><input type="text" name="nome" value=""></td>
+				<td>Nome: </td>
+				<td><input type="text" name="nome" value=""></td>	
 			</tr>
 			<tr>
-				<td>Nascimento:</td>
-				<td><input type="date" name="datanas" value=""></td>
+				<td>E-mail: </td>
+				<td><input type="text" name="email" value=""></td>
 			</tr>
 			<tr>
-				<td>Telefone:</td>
-				<td><input type="text" name="tel" value=""></ td>
+				<td>Endereço: </td>
+				<td><input type="text" name="endereco" value=""></td>
 			</tr>
 			<tr>
-				<td>Endereço:</td>
-				<td><input type="text" name="end" value=""></td>
+				<td>Data de nascimento: </td>
+				<td><input type="date" name="data" value=""></td>
 			</tr>
 			<tr>
 				<td><input type="hidden" name="acao" value="inserir"></td>
-				<td><input type="submit" name="Enviar" value="Enviar"></td>
+				<td><input type="submit" name="entra" value="Entra"></td>
 			</tr>
 		</table>
-	</form></td></center>
+	</form>	
+
+	</center>
 </body>
 </html>
