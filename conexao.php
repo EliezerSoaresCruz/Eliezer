3230a5aee4c8f2283f7251708c689a6223a18c36
EliezerSoaresCruz <48848565+EliezerSoaresCruz@users.noreply.github.com>
2019-03-23 15:40:15 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 if ($_POST['acao']=='inserir') {
 	$data = date('d-m-Y',strtotime($_POST['data']));
-	print('<b>Seu nome é: '.$_POST['nome'].'<br>');
-	print('Seu e-mail é: '.$_POST['email'].'<br>');
-	print('Seu endereço é: '.$_POST['endereco'].'<br>');
-	print('Nasceu em: '.$data.'</b>');
+	echo('<b>Seu nome é: '.$_POST['nome'].'<br>');
+	echo('Seu e-mail é: '.$_POST['email'].'<br>');
+	echo('Seu endereço é: '.$_POST['endereco'].'<br>');
+	echo('Nasceu em: '.$data.'</b>');
 }
 ?>
